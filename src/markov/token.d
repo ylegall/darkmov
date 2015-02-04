e679781f0dc06b2606d7e83c0201b324473136c2
Yann Le Gall <ylegall@gmail.com>
2015-02-03 19:39:33 -0800

    add configurable regex pattern

---
+++ src/markov/token.d
@@ -0,0 +1,44 @@
+
+module markov.token;
+
+import markov;
+
+alias string Token;
+alias Appender!(Token[]) TokenList;
+alias Node*[Token] NodeMap;
+
+enum DEFAULT_PATTERN = `([\w\.\?!;]+)`;
+enum DEFAULT_REGEX = ctRegex!(DEFAULT_PATTERN);
+
+auto tokenize(T)(T line)
+{
+    return matchAll(line, DEFAULT_REGEX).map!(a => a.hit.idup);
+}
+
+auto tokenize(T)(T line, StaticRegex!char pattern)
+{
+    return matchAll(line, pattern).map!(a => a.hit.idup);
+}
+
+auto tokenize(T)(T line, Regex!char pattern)
+{
+    return matchAll(line, pattern).map!(a => a.hit.idup);
+}
+
+auto randomToken(NodeMap map, Random rand)
+{
+    auto keys = map.keys;
+    return keys[uniform(0, keys.length, rand)];
+}
+
+auto randomToken(TokenList list, Random rand)
+{
+    return list.data[uniform(0, list.data.length, rand)];
+}
+
+unittest
+{
+    assert(equal(tokenize("one two. 'three' four? five six!", DEFAULT_REGEX),
+                ["one", "two.", "three", "four?", "five", "six!"]
+                ));
+}
